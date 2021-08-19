f1f7bc513088165c1512ba78bd5f7465278ee9c8
944814039yangguodong <944814039@qq.com>
2021-08-19 17:43:33 +0800

    前端基础页面部分功能实现第二版

--- src/test/java/cn/ncepu/mydeveloping/CodeGenerator.java
+++ src/test/java/cn/ncepu/mydeveloping/CodeGenerator.java
@@ -62,7 +62,7 @@ public class CodeGenerator {
 
         // 5、策略配置
         StrategyConfig strategy = new StrategyConfig();
-        strategy.setInclude("project");
+        strategy.setInclude("time");
         strategy.setNaming(NamingStrategy.underline_to_camel);//数据库表映射到实体的命名策略
         //strategy.setTablePrefix("lab"+ "_"); //生成实体时去掉表前缀
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);//数据库表字段映射到实体的命名策略
